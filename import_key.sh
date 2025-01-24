6da37981e07753e6ec6d55c2087d09407f6aca26
tris002xx <tristanjames3131@gmail.com>
2025-01-24 14:50:48 -0800

    import_key fixing

--- import_key.sh
+++ import_key.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+KEY_NAME="bcitkey"
+
+# Generate a private key
+ssh-keygen -t rsa -b 4096 -f ${KEY_NAME}.pem 
 
 # Generate a public key from the private key
-ssh-keygen -y -f bcitkey.pem > bcitkey.pub
-
+ssh-keygen -y -f ${KEY_NAME}.pem > ${KEY_NAME}.pub
 
 # Import the public key to the AWS EC2 instance
-aws ec2 import-key-pair --key-name "bcitkey" --public-key-material file://bcitkey.pub
+aws ec2 import-key-pair --key-name "${KEY_NAME}" --public-key-material file://${KEY_NAME}.pub
 
 if [ $? -eq 0 ]; then
-  echo "Key pair '$KEY_NAME' successfully imported into AWS."
+  echo "Key pair '${KEY_NAME}' successfully imported into AWS."
 else
-  echo "Failed to import key pair '$KEY_NAME'."
+  echo "Failed to import key pair '${KEY_NAME}'."
 fi
